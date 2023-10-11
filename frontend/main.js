784655215ed6f81c9ba1d0024185642609a93d04
RobK3662 <robert_907@gmx.de>
2023-10-11 11:51:31 +0200

    Update

--- frontend/main.js
+++ frontend/main.js
@@ -126,6 +126,7 @@ const tableData = [
 const itemsPerPage = 6;
 
 // Funktion zum Anzeigen der Tabelle basierend auf der aktuellen Seite
+
 function displayTable(pageNumber) {
     const table = document.querySelector(".table tbody");
     table.innerHTML = "";
@@ -189,3 +190,53 @@ function createPaginationButtons() {
 // Initialisieren Tabelle und die Paginierung
 displayTable(1);
 createPaginationButtons();
+
+/*
+
+// Erstelle eine leere Tabelle
+function createEmptyTable() {
+    const table = document.getElementById("leaderboardData");
+    table.innerHTML = "";
+}
+
+// Lade die Ranglisten-Daten 
+function loadLeaderboard() {
+    const apiUrl = "https://api.chess.com/pub/leaderboards";
+
+    // Führen Sie die API-Anfrage mit Fetch durch
+    fetch(apiUrl)
+        .then((response) => response.json())
+        .then((data) => {
+            // Überprüfen, ob data.leaderboards existiert und mindestens ein Element enthält
+            if (data.leaderboards && data.leaderboards.length > 0) {
+                const tableData = data.leaderboards[0].rankings;
+
+                const table = document.getElementById("leaderboardData");
+
+                // Leere die Tabelle zuerst
+                table.innerHTML = "";
+
+                for (const rowData of tableData) {
+                    const row = document.createElement("tr");
+                    row.innerHTML = `
+                        <td>${rowData.rank}</td>
+                        <td><img src="${rowData.avatar}" alt="Avatar" class="h-16 w-16 rounded-full" /></td>
+                        <td>${rowData.username}</td>
+                        <td>${rowData.score}</td>
+                        <td>${rowData.title}</td>
+                        <td>${rowData.country}</td>
+                        <td>${rowData.wins}</td>
+                        <td>${rowData.losses}</td>
+                        <td>${rowData.draws}</td>
+                    `;
+                    table.appendChild(row);
+                }
+            } else {
+                console.error("API-Antwort enthält keine Leaderboard-Daten.");
+            }
+        })
+        .catch((error) => {
+            console.error("Fehler beim Abrufen der Daten:", error);
+        });
+}
+*/
